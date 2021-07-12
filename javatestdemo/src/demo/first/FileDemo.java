93a2798285257a977a100166ba3ff83c48f66d96
sevenskyday <295jj@163.com>
2021-07-12 17:38:49 +0800

    提交

--- javatestdemo/src/demo/first/FileDemo.java
+++ javatestdemo/src/demo/first/FileDemo.java
@@ -1,47 +1,54 @@
 package demo.first;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 
 public class FileDemo {
 	public static void main(String[] args) throws IOException {
+		
+		
 		// windows 分隔符"\"
 		// linux 分隔符 "/"
 		// public static final String separaor
-		// File file=new File("C:"+File.separator+"Users\\XRNY\\Desktop\\test.txt");
-		File file = new File("C:" + File.separator);
-		if (file.isDirectory()) {
-			File result[] = file.listFiles();
-			for (int x = 0; x < result.length; x++) {
-				System.out.println(result[x]);
-			}
-		} else {
+		File file=new File("C:"+File.separator+"Users\\XRNY\\Desktop\\test.txt");
+		//File file = new File("C:" + File.separator);
+//		if (file.isDirectory()) {
+//			File result[] = file.listFiles();
+//			for (int x = 0; x < result.length; x++) {
+//				System.out.println(result[x]);
+//			}
+//		} else {
 			if (!file.getParentFile().exists()) {
 				file.getParentFile().mkdirs();
 			}
 
-			if (file.exists()) {
-				file.delete();
-
-			} else {
-				System.out.println(file.createNewFile());
-				System.out.println(file.canRead());
-				System.out.println(file.canWrite());
-				// 文件大小返回字节
-				System.out.println(MathUtil.round(file.length() / (1024 * 1024), 2));
-
-				System.out.println(DateUtil.ToyyyMMddHHmmss(file.lastModified()));
-
-				System.out.println(file.isDirectory());
-
-				System.out.println(file.isFile());
-			}
-
-		}
-
+			OutputStream output=new FileOutputStream(file);
+			String str="www.baidu.com";
+			output.write(str.getBytes());
+			output.close();
+			
+			
+//			if (file.exists()) {
+//				file.delete();
+//
+//			} else {
+//				System.out.println(file.createNewFile());
+//				System.out.println(file.canRead());
+//				System.out.println(file.canWrite());
+//				// 文件大小返回字节
+//				System.out.println(MathUtil.round(file.length() / (1024 * 1024), 2));
+//
+//				System.out.println(DateUtil.ToyyyMMddHHmmss(file.lastModified()));
+//
+//				System.out.println(file.isDirectory());
+//
+//				System.out.println(file.isFile());
+//			}
+			//}
 	}
-
 }
 
 class FileUtil {
